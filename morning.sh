ab36814cc97348d4b8aecf9ee3ca86612c0a45f8
deda9 <Bassem.Qoulta@gmail.com>
2018-11-29 17:12:28 +0100

    initial commit

---
+++ morning.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+open -a "Slack"
+open -a "Google Chrome" https://mail.google.com/mail/u/0/#inbox #Work Gmail
+open -a "Google Chrome" https://mail.google.com/mail/u/1/#inbox #Personal Gmail
