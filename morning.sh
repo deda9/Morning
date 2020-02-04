50b95879ca1053d4ea20703828c71c634fe5e480
Bassem Qoulta <bassem.qoulta@gmail.com>
2020-02-04 10:38:08 +0100

    Update morning.sh

--- morning.sh
+++ morning.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+open -a Xcode /path/to.xcworkspace
 open -a "Slack"
 open -a "Google Chrome" https://mail.google.com/mail/u/0/#inbox #Work Gmail
 open -a "Google Chrome" https://mail.google.com/mail/u/1/#inbox #Personal Gmail
